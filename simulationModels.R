e86d85d7d2b5bce32fb3f67b1a36325475a7dc23
Theodoulos Rodosthenous <45739717+theorod93@users.noreply.github.com>
2019-10-09 16:59:48 +0100

    Create simulationModels.R

---
+++ simulationModels.R
@@ -0,0 +1,5 @@
+## This is a script for the data generating models used in this study
+## It consists of the function(s) implementing two or more data-sets in each data generating model, 
+## as well as a single function calling all three models
+
+
